1d8c7f846c000a4c0510e6dda3cccf21d0eab130
Olivier OS <olivos248@hotmail.com>
2019-03-14 15:50:36 -0400

    v1

--- Net.h
+++ Net.h
@@ -17,6 +17,7 @@ public:
 	Net(const std::vector<int> & layers); /* input a vector with the sizes of the layers, starting with the largest layer : l1 , l2 , lk */
 	double & c(const int & layer, const int & start, const int & end); /* gives access to a coefficient, starting from the layer given as input, enables it to be modified */
 	double & v(const int & layer, const int & i); /* gives access to a the node value from the layer given as input, enables it to be modified */
+	arma::vec & get_coeffs(); /* returns reference to the whole vector of coefficients to be used for the gradient descent */
 	virtual ~Net();
 private:
 arma::vec coeffs; /* coefficients : vector of size  ( l1*l2 + l2*l3 + ... + l{k-1}*lk ) */
