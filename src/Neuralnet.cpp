69707a84ab3c2b115039adbf5b8b665e55de9649
Olivier OS <olivos248@hotmail.com>
2019-03-25 16:00:15 -0400

    v3

--- src/Neuralnet.cpp
+++ src/Neuralnet.cpp
@@ -54,10 +54,13 @@ Net grad(const Net & N,const double & target){
 	return G;
 }
 
-arma::mat grad_descent(const arma::mat& v0, std::function<arma::mat(const arma::mat &)> & grad,const double & eth,const double & eps){
+arma::mat grad_descent(const arma::mat& v0, std::function<arma::mat(const arma::mat &)> & grad,const double & etha,const double & eps){
 	mat v{v0};
 	mat g = grad(v);
-	const int maxIt{100};
+	double old_n = norm(g);
+	double eth = etha;
+	double temp = old_n;
+	const int maxIt{10000};
 	for(int i = 0 ; i != maxIt ; ++i ){
 		if(norm(g) < eps){
 			cout << "numit" << i <<' ' << norm(g) <<'\n';
@@ -66,10 +69,62 @@ arma::mat grad_descent(const arma::mat& v0, std::function<arma::mat(const arma::
 		else{
 			v = v-eth*g;
 			g = grad(v);
+			temp = norm(g);
+			if(temp > old_n){
+				eth /= 10.;
+			}
+			old_n = temp;
+//			cout << norm(g) << endl;
 		}
 	}
 	cout << "reached max it" << norm(g) << '\n';
 	return v;
+}
+
+//arma::mat grad_descent(const arma::mat& v0, std::function<arma::mat(const arma::mat &)> & grad,const double & eth,const double & eps){
+//	mat v{v0};
+//	mat g = grad(v);
+//	const int maxIt{1000};
+//	for(int i = 0 ; i != maxIt ; ++i ){
+//		if(norm(g) < eps){
+//			cout << "numit" << i <<' ' << norm(g) <<'\n';
+//			return v;
+//		}
+//		else{
+//			v = v-eth*g;
+//			g = grad(v);
+////			cout << norm(g) << endl;
+//		}
+//	}
+//	cout << "reached max it" << norm(g) << '\n';
+//	return v;
+//}
+
+
+arma::mat acc_descent(const arma::mat& v0, std::function<arma::mat(const arma::mat &)> & grad,const double & eth,const double & eps){
+	mat x{v0};
+	mat y{v0};
+	mat y_old{v0};
+	mat g = grad(v0);
+	double s{0};
+	const int maxIt{100};
+	for(int i = 0 ; i != maxIt ; ++i ){
+		if(norm(g) < eps){
+			cout << "numit" << i <<' ' << norm(g) <<'\n';
+			return x;
+		}
+		else{
+			s = ((double)i)/(i+3.);
+			y = x - eth*g;
+			x = x*(1+s)-eth*g*(1+s)-s*y_old;
+			g = grad(x);
+			y_old = y;
+			cout << norm(g)<<endl;
+		}
+	}
+	cout << "reached max it" << norm(g) << '\n';
+	return x;
+
 }
 
 
@@ -77,96 +132,48 @@ arma::mat grad_descent(const arma::mat& v0, std::function<arma::mat(const arma::
 int main(){
 //	Data processing
 
-	const int nassets{10};
+	const int nassets{487};
 	const int nlines{756};
-	const int end_train{10};
+	const double end_train{10};
 	dataframe Data{756,nassets,"cleanIndex.csv"};
 	mat Train = Data.getData().rows(0,end_train);
 
-
 //	Define fully connected neural net : layer0 : 500 nodes / layer 1 : 250  nodes / layer 2 : 125 nodes / layer 3 : 1 node
 
-//	For layer 0 we will add 0 because we only have 486 assets.
+//	For layer 0 we will add 0s because we only have 486 assets.
 
-
-//	Net N = Net(vector<int>{500,250,125,1});
-//	Net G = grad(N,0); /* we do not care of the target we just want the structure*/
-
-
-	std::function<arma::vec(const arma::vec & )> g = [Train](const arma::vec & v){
-	Net N = Net(vector<int>{10,5,2,1});
-	Net G = Net(vector<int>{10,5,2,1}); /* we do not care of the target we just want the structure*/
+	Net N = Net(vector<int>{1000,500,125,1});
+	Net G = Net(vector<int>{1000,500,125,1}); /* we do not care of the target we just want the structure*/
 	vec res_grad = vec(G.get_coeffs().n_rows,fill::zeros);
+
+	std::function<arma::mat(const arma::mat & )> g = [&Train,&N,&G,&res_grad,&end_train](const arma::mat & v){
 	N.get_coeffs() = v;
+	res_grad.fill(0.);
 	for(int d = 0 ; d != end_train+1-10 ; ++d){
 //		For each available date, we calculate a gradient and then we average
 		N.v(0,0) = 0;
 		for(int s = 1 /* do not use the current index price*/ ; s != nassets ; ++s){
 			N.v(0,s) = Train(d,s);
-//			cout << s<<endl;
-
 		}
-//		for(int s = nassets; s != 500 ; ++s){
-//			N.v(0,s) = 0;
-////			cout << N.v(0,s)<<endl;
-//		}
-
 		N.update();
-		G = grad(N,Train(0,d+10));
+		G = grad(N,Train(d+10,0));
 		res_grad += G.get_coeffs();
-
 	}
-//	cout <<  res_grad<<endl;
-	return res_grad;
+//	cout << (end_train+1-10)<<endl;
+	return res_grad/(end_train+1-10);
 	};
-	cout <<1 <<endl;
+	Net T = Net(vector<int>{1000,500,125,1}); /* we do not care of the target we just want the structure*/
 
-	vec v0 = vec(10*5+5*2+2,fill::zeros);
-	cout << g(v0);
-	cout<<1<<endl;
+	vec v0 = vec(T.get_coeffs().n_rows,fill::randn);
 
-//	cout << gradi.n_rows;
-//	cout << grad_descent(N.get_coeffs(),g,0.0001,0.01)<<endl;
-//	cout << N.v(N.L().size()-1,0);
+	auto start = std::chrono::high_resolution_clock::now();
+	grad_descent(v0,g,0.000000001,0.01);
+	auto finish = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed = finish - start;
 
+	cout << N.v(3,0)<< ' ' << Train(10,0)<<' '<< elapsed.count();
 	return 0;
 }
-
-
-//	Net check_grad = Net(vector<int>{8,4,2,1});
-//	const double target{0.};
-//	const double v = pow(N.v(4-1,0)-target,2);
-//	double vbis{0};
-//	const double eps{0.00000001};
-//
-//	for(int l = 0 ; l != N.L().size() -1 ; ++l ){
-//		for(int s = 0 ; s != N.L()[l] ; ++s ){
-//			for(int e = 0 ; e != N.L()[l+1]; ++e){
-//				N.c(l,s,e) += eps; /* node 0 of layer 0 */
-//				N.update();
-//				vbis = pow(N.v(N.L().size()-1,0)-target,2);
-//				N.c(l,s,e) -= eps; /* node 0 of layer 0 */
-//				N.update();
-//				check_grad.c(l,s,e) = (vbis-v)/eps;
-//			}
-//		}
-//	}
-//
-//	Net G = grad(N,target);
-//
-//	cout << G.get_coeffs() -check_grad.get_coeffs();
-
-
-//	N.update();
-//	cout <<"before descent" <<N.v(3,0)<<endl;
-//	for(int i = 0 ; i != 100 ; ++i){
-//	Net G = grad(N,0);
-//	N.get_coeffs() -= 0.0001*G.get_coeffs();
-//	N.update();
-//	cout <<N.v(3,0)<<endl;
-//
-//	}
-//	N.print();
 
 
 
