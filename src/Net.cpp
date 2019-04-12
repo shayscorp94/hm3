a86e5f1b8188b3d4809f8b183dd09065000e7ff5
Olivier OS <olivos248@hotmail.com>
2019-04-12 16:47:39 -0400

    aks

--- src/Net.cpp
+++ src/Net.cpp
@@ -1,10 +1,3 @@
-/*
- * Net.cpp
- *
- *  Created on: Mar 13, 2019
- *      Author: oliv
- */
-
 #include "Net.h"
 #include "math.h"
 #include <random>
@@ -12,105 +5,105 @@ using namespace std;
 using namespace arma;
 
 
-Net::Net(const std::vector<int> & layers, std::vector<double (*)(const double&)> fs)
-:layers(layers),fs(fs)
+Net::Net(const std::vector<int> & layers, std::vector<double(*)(const double&)> fs)
+	:layers(layers), fs(fs)
 {
-	int n_coeffs{0};
-	int n_nodes{0};
-	int temp{1};
+	int n_coeffs{ 0 };
+	int n_nodes{ 0 };
+	int temp{ 1 };
 
-	for( vector<int>::const_iterator it = layers.begin(); it != layers.end() ; ++it){
-		if(it == layers.begin()){
+	for (vector<int>::const_iterator it = layers.begin(); it != layers.end(); ++it) {
+		if (it == layers.begin()) {
 			temp = *it;
 			n_nodes += temp;
 		}
-		else{
-			n_coeffs += temp * *it;
+		else {
+			n_coeffs += temp ; //change 1
 			temp = *it;
 			n_nodes += temp;
 		}
 	}
-//	coeffs = vec(n_coeffs, fill::randn);
-////	cout << coeffs(0)<<endl;
-//	temp = 0;
-//	for (vector<int>::const_iterator it = layers.begin(); it != layers.end(); ++it) {
-//		if (it == layers.begin()) {
-//		}
-//		else {
-//			coeffs.rows(temp, temp + *it * *(it - 1) - 1) *= sqrt(2./ *(it - 1));
-////			cout << 1/ *(it - 1) << endl;
-//			temp += *it * *(it - 1);
-//		}
-//	}
+	//	coeffs = vec(n_coeffs, fill::randn);
+	////	cout << coeffs(0)<<endl;
+	//	temp = 0;
+	//	for (vector<int>::const_iterator it = layers.begin(); it != layers.end(); ++it) {
+	//		if (it == layers.begin()) {
+	//		}
+	//		else {
+	//			coeffs.rows(temp, temp + *it * *(it - 1) - 1) *= sqrt(2./ *(it - 1));
+	////			cout << 1/ *(it - 1) << endl;
+	//			temp += *it * *(it - 1);
+	//		}
+	//	}
 	coeffs = vec(n_coeffs, fill::zeros);
 	mt19937 g;
 	std::normal_distribution<double> d;
-//	cout << coeffs(0)<<endl;
+	//	cout << coeffs(0)<<endl;
 	temp = 0;
 	for (vector<int>::const_iterator it = layers.begin(); it != layers.end(); ++it) {
 		if (it == layers.begin()) {
 		}
 		else {
-			for(int i = temp ; i != temp + *it * *(it - 1) ;++i ){
-			coeffs(i) = d(g)*sqrt(2./ *(it - 1));
+			for (int i = temp; i != temp + *(it - 1); ++i) { //change 2
+				coeffs(i) = d(g)*sqrt(2. / *(it - 1));
 			}
-//			cout << 1/ *(it - 1) << endl;
-			temp += *it * *(it - 1);
+			//			cout << 1/ *(it - 1) << endl;
+			temp += *(it - 1); //change 3
 		}
 	}
-//	cout << coeffs(0)<<endl;
+	//	cout << coeffs(0)<<endl;
 
-	nodevals = vec(n_nodes,fill::zeros);
-	nodes = vec(n_nodes,fill::zeros);
+	nodevals = vec(n_nodes, fill::zeros);
+	nodes = vec(n_nodes, fill::zeros);
 
 }
 
 double& Net::c(const int& layer_num, const int& start, const int& end) {
-	int cumsum{0};
-	for(int i = 1; i !=layer_num+1;++i){
-		cumsum += layers[i]*layers[i-1];
+	int cumsum{ 0 };
+	for (int i = 1; i != layer_num + 1; ++i) {
+		cumsum += layers[i - 1];  //change 4
 	}
-	return coeffs(cumsum+start*layers[layer_num+1]+end);
+	return coeffs(cumsum + start); //change 5
 }
 
-double Net::c(const int& layer_num, const int& start, const int& end) const{
-	int cumsum{0};
-	for(int i = 1; i !=layer_num+1;++i){
-		cumsum += layers[i]*layers[i-1];
+double Net::c(const int& layer_num, const int& start, const int& end) const {
+	int cumsum{ 0 };
+	for (int i = 1; i != layer_num + 1; ++i) {
+		cumsum += layers[i - 1]; //change 6
 	}
-	return coeffs(cumsum+start*layers[layer_num+1]+end);
+	return coeffs(cumsum + start); //change 7
 }
 
 double& Net::v(const int& layer_num, const int& i) {
-	int cumsum{0};
-	for(int i = 0; i !=layer_num;++i){
+	int cumsum{ 0 };
+	for (int i = 0; i != layer_num; ++i) {
 		cumsum += layers[i];
 	}
-	return nodevals(cumsum+i);
+	return nodevals(cumsum + i);
 }
 
 double Net::v(const int& layer_num, const int& i) const {
-	int cumsum{0};
-	for(int i = 0; i !=layer_num;++i){
+	int cumsum{ 0 };
+	for (int i = 0; i != layer_num; ++i) {
 		cumsum += layers[i];
 	}
-	return nodevals(cumsum+i);
+	return nodevals(cumsum + i);
 }
 
 double& Net::n(const int& layer_num, const int& i) {
-	int cumsum{0};
-	for(int i = 0; i !=layer_num;++i){
+	int cumsum{ 0 };
+	for (int i = 0; i != layer_num; ++i) {
 		cumsum += layers[i];
 	}
-	return nodes(cumsum+i);
+	return nodes(cumsum + i);
 }
 
 double Net::n(const int& layer_num, const int& i) const {
-	int cumsum{0};
-	for(int i = 0; i !=layer_num;++i){
+	int cumsum{ 0 };
+	for (int i = 0; i != layer_num; ++i) {
 		cumsum += layers[i];
 	}
-	return nodes(cumsum+i);
+	return nodes(cumsum + i);
 }
 
 arma::vec& Net::get_coeffs() {
@@ -118,43 +111,51 @@ arma::vec& Net::get_coeffs() {
 }
 
 void Net::print() {
-	int temp{0};
-	cout << "VALUES"<<endl;
-	for( vector<int>::const_iterator it = layers.begin(); it != layers.end() ; ++it){
-			cout << nodevals.rows(temp,temp+*it-1)<<endl;
-			temp += *it;
+	int temp{ 0 };
+	cout << "VALUES" << endl;
+	for (vector<int>::const_iterator it = layers.begin(); it != layers.end(); ++it) {
+		cout << nodevals.rows(temp, temp + *it - 1) << endl;
+		temp += *it;
 	}
 	temp = 0;
-	cout << "COEFFICIENTS"<<endl;
-	for( vector<int>::const_iterator it = layers.begin(); it != layers.end() ; ++it){
-		if(it == layers.begin()){
+	cout << "COEFFICIENTS" << endl;
+	for (vector<int>::const_iterator it = layers.begin(); it != layers.end(); ++it) {
+		if (it == layers.begin()) {
 		}
-		else{
-			cout << coeffs.rows(temp,temp+ *it * *(it-1) -1)<<endl;
-			temp += *it * *(it-1);
+		else {
+			cout << coeffs.rows(temp, temp + *(it - 1) - 1) << endl; //change 8
+			temp += *(it - 1); //change 9
 		}
 	}
 }
 
 void Net::update() {
-	if(layers.size() >0){
-		for(int end = 0 ; end != layers[0] ; ++end){
-		v(0,end) = fs[0](n(0,end)); /* function of layer */
-//		cout << n(0,end)<<endl;
+	if (layers.size() > 0) {
+		for (int end = 0; end != layers[0]; ++end) {
+			v(0, end) = fs[0](n(0, end)); /* function of layer */
+	//		cout << n(0,end)<<endl;
 		}
 	}
-	for(int l = 1 ; l != layers.size() ; l++){
-		for(int end = 0 ; end != layers[l] ; ++end){
-			n(l,end) = 0;
-			for(int start = 0 ; start != layers[l-1] ; ++start){
-				n(l,end) += c(l-1,start,end)*v(l-1,start);
-			}
-			v(l,end) = fs[l](n(l,end)); /* function of layer */
+	for (int l = 1; l != layers.size() - 1; l++) { //change 9 - remember to change for the next column
+		int start = 0;
+		for (int end = 0; end != layers[l]; ++end) {
+			n(l, end) = 0;
+			n(l, end) = c(l - 1, start, end)*v(l - 1, start) + c(l - 1, start + 1, end)*v(l - 1, start + 1); //change 10
+			v(l, end) = fs[l](n(l, end)); /* function of layer */ //change 11
+			start += 2; // change 12
 		}
+	}
+	// Now l = layers.size() - 1  //change 13
+	int l = layers.size() - 1;
+	for (int end = 0; end != layers[l]; ++end) {
+		n(l, end) = 0;
+		for (int start = 0; start != layers[l - 1]; ++start) {
+			n(l, end) += c(l - 1, start, end)*v(l - 1, start);
+		}
+		v(l, end) = fs[l](n(l, end)); /* function of layer */
 	}
 }
 
 Net::~Net() {
 	// TODO Auto-generated destructor stub
 }
-
--- src/Neuralnet.cpp
+++ src/Neuralnet.cpp
@@ -1,148 +1,129 @@
-/*
- * net.cpp
- *
- *  Created on: Mar 13, 2019
- *      Author: oliv
- */
-
-
 #include <iostream>
 #include <armadillo>
 #include <algorithm>    // std::random_shuffle
-#include <random>
-
 
 #include "Net.h"
-#include "math.h"
 #include "dataframe.h"
 
 using namespace std;
 using namespace arma;
 using namespace vSpace;
 
-inline double relu(const double & d){
+double relu(const double & d) {
 	return d > 0 ? d : 0;
 }
-inline double Lrelu(const double & d){
+
+double Lrelu(const double & d) {
 	return d > 0 ? d : 0.01*d;
 }
-inline double DLrelu(const double & d){
+
+double DLrelu(const double & d) {
 	return d > 0 ? 1 : 0.01;
 }
-inline double I(const double & t){
+
+
+
+double I(const double & t) {
 	return t;
 }
-inline double One(const double & t){
+
+double One(const double & t) {
 	return 1;
 }
-inline double Drelu(const double & d){
+
+double Drelu(const double & d) {
 	return d > 0 ? 1 : 0;
 }
-inline double sm(const double & t){
-	return (t>10)?t:log(1+exp(t));
-}
-inline double Dsm(const double & t){
 
-	return (t>100)?1:exp(t)/(1+exp(t));
-}
 
-inline void grad(const Net & N,Net & G,const double & target){
+inline void grad(const Net & N, Net & G, const double & target) { //changes made inside
 
 	const vector<int> & layers = N.L();
 	const int n_layers = layers.size();
-	const vector<double (*)(const double &)> deriv=G.getFs();
-//	Deal with end node if net is not empty
-	if(n_layers > 0){
-//		cout << N.n(n_layers-1,0) << endl;
-		G.v(n_layers-1,0) = 2*deriv[n_layers-1](N.n(n_layers-1,0))*( N.v(n_layers-1,0)  -target);
-//		cout <<deriv[n_layers-1](N.n(n_layers-1,0))<<endl;
+	const vector<double(*)(const double &)> deriv = G.getFs();
+	//	Deal with end node if net is not empty
+	if (n_layers > 0) {
+		G.v(n_layers - 1, 0) = 2 * deriv[n_layers - 1](N.n(n_layers - 1, 0))*(N.v(n_layers - 1, 0) - target);
 	}
 
-	for(int l = n_layers-2 ; l != -1 ; --l){
-//		Coeffs of G store partial diff with respect to that coeff
-		for(int start = 0 ; start != layers[l] ; ++start){
-			for(int end = 0; end != layers[l+1]; ++end){
-				G.c(l,start,end) = N.v(l,start)*G.v(l+1,end);
+	for (int l = n_layers - 2; l != -1; --l) {
+		//		Coeffs of G store partial diff with respect to that coeff
+
+		if (l == n_layers - 2) {
+			for (int start = 0; start != layers[l]; ++start) {
+				G.v(l, start) = 0;
+				for (int end = 0; end != layers[l + 1]; ++end) {
+					G.c(l, start, end) = N.v(l, start)*G.v(l + 1, end);
+					G.v(l, start) += N.c(l, start, end)*deriv[l](N.v(l, start))*G.v(l + 1, end);
+				}
+			}
+
+		}
+		else
+		{
+			int end = 0;
+			for (int start = 0; start != layers[l]; start = start + 2) {
+					G.c(l, start, end) = N.v(l, start)*G.v(l + 1, end);
+					G.c(l, (start + 1), end) = N.v(l, (start + 1))*G.v(l + 1, end);
+					end++;
+			}
+
+			end = 0;
+			for (int start = 0; start != layers[l]; start = start + 2) {
+			//	G.v(l, start) = 0;
+			//	G.v(l, start + 1) = 0;
+				G.v(l, start) = N.c(l, start, end)*deriv[l](N.v(l, start))*G.v(l + 1, end);
+				G.v(l, (start + 1)) = N.c(l,(start + 1), end)*deriv[l](N.v(l,(start + 1)))*G.v(l + 1, end);
+				end++;
 			}
 		}
-//		Nodes of G store partial diff with respect to that node value
-		for(int start = 0 ; start != layers[l] ; ++start){
-			G.v(l,start) = 0;
-			for(int end = 0; end != layers[l+1]; ++end){
-				G.v(l,start) += N.c(l,start,end)*deriv[l](N.n(l,start))*G.v(l+1,end);
-			}
-		}
+
+		//		Nodes of G store partial diff with respect to that node value
+
 	}
 }
 
-static const int nassets{487};
-static const int nlines{756};
-static const double end_train{10};
-static dataframe Data{756,nassets,"cleanIndex.csv"};
-static mat Train = Data.getData().rows(0,end_train+0);
-static const int batchSize{1};
-
-
-vector<double (*)(const double &)> fs{I,Lrelu,Lrelu,Lrelu};
-vector<double (*)(const double &)> ds{One,DLrelu,DLrelu,DLrelu};
-vector<Net> nets;
-Net G = Net(vector<int>{487,250,125,1},ds); /* we do not care of the target we just want the structure*/
-vec res_grad = vec(G.get_coeffs().n_rows,fill::zeros);
-
-arma::mat g(const arma::mat & v){
-res_grad.fill(0.);
-for(int d = 0 ; d != end_train+1-10 ; ++d){
-	nets[d].get_coeffs() = v;
-	nets[d].update();
-	grad(nets[d],G,Train(d+10,0));
-	res_grad += G.get_coeffs();
-}
-return res_grad/(end_train+1-10);
-};
-
-
-arma::mat grad_descent(const arma::mat & v0, arma::mat (& grad) (const arma::mat &), const double & etha,const double & eps){
-	mat v{v0};
+arma::mat grad_descent(const arma::mat& v0, std::function<arma::mat(const arma::mat &)> & grad, const double & etha, const double & eps) {
+	mat v{ v0 };
 	mat g = grad(v);
-//	double old_n = norm(g);
+	//	double old_n = norm(g);
 	double eth = etha;
-//	double temp = old_n;
-	const int maxIt{300};
-	for(int i = 0 ; i != maxIt ; ++i ){
-		if(norm(g) < eps){
-			cout << "numit" << i <<' ' << norm(g) <<'\n';
+	//	double temp = old_n;
+	const int maxIt{ 1000000 };
+	for (int i = 0; i != maxIt; ++i) {
+		if (norm(g) < eps) {
+			cout << "numit" << i << ' ' << norm(g) << '\n';
 			return v;
 		}
-		else{
-			v = v-eth*g;
+		else {
+			v = v - eth * g;
 			g = grad(v);
-//			cout << "grad" << norm(g) << endl;
+			//			cout << "grad" << norm(g) << endl;
 		}
 	}
 	cout << "reached max it" << norm(g) << '\n';
 	return v;
 }
 
-
-arma::mat acc_descent(const arma::mat& v0 , std::function<arma::mat(const arma::mat &)> & grad,const double & eth,const double & eps){
-	mat x{v0};
-	mat y{v0};
-	mat y_old{v0};
+arma::mat acc_descent(const arma::mat& v0, std::function<arma::mat(const arma::mat &)> & grad, const double & eth, const double & eps) {
+	mat x{ v0 };
+	mat y{ v0 };
+	mat y_old{ v0 };
 	mat g = grad(v0);
-	double s{0};
-	const int maxIt{100};
-	for(int i = 0 ; i != maxIt ; ++i ){
-		if(norm(g) < eps){
-			cout << "numit" << i <<' ' << norm(g) <<'\n';
+	double s{ 0 };
+	const int maxIt{ 100 };
+	for (int i = 0; i != maxIt; ++i) {
+		if (norm(g) < eps) {
+			cout << "numit" << i << ' ' << norm(g) << '\n';
 			return x;
 		}
-		else{
-			s = ((double)i)/(i+3.);
-			y = x - eth*g;
-			x = x*(1+s)-eth*g*(1+s)-s*y_old;
+		else {
+			s = ((double)i) / (i + 3.);
+			y = x - eth * g;
+			x = x * (1 + s) - eth * g*(1 + s) - s * y_old;
 			g = grad(x);
 			y_old = y;
-//			cout << norm(g)<<endl;
+			//			cout << norm(g)<<endl;
 		}
 	}
 	cout << "reached max it" << norm(g) << '\n';
@@ -150,126 +131,67 @@ arma::mat acc_descent(const arma::mat& v0 , std::function<arma::mat(const arma::
 
 }
 
-arma::mat stochastic_descent(const arma::mat& v0, std::function<arma::mat(const arma::mat&,mt19937 &)> & grad,const double & etha,const double & eps){
-	mat v{v0};
-	const int maxIt{400};
-	mt19937 gen;
-	mt19937 genB;
-	uniform_int_distribution<int> Dist(0,::end_train-10);
-	mat g = grad(v,gen);
-	double eth = etha;
-	double lmin = 0;
-	double min = -1 ;
-	double lin = 0.1;
-	double err = 0;
-	int d{0};
-	const int n_layers = 4;
+int main() {
+	//	Data processing
 
-	for(int i = 0 ; i != maxIt ; ++i ){
-		if(norm(g) < eps){
-			cout << "numit" << i <<' ' << norm(g) <<'\n';
-			return v;
-		}
-		else{
-			lmin = 0;
-			min = -1 ;
-			lin = 0.1;
-			for(int j = 0 ; j != 3 ; ++j){
-				err = 0;
+	const int nassets{ 484 };
+	const int nlines{ 756 };
+	const double end_train{ 10};
+	dataframe Data{ 756,nassets,"cleanIndex.csv" };
+	mat Train = Data.getData().rows(0, end_train);
 
-				for(int k = 0 ; k != batchSize ; ++k){
-				d = Dist(genB);
-				nets[d].get_coeffs() = v-eth*lin*g;
-				nets[d].update();
-				err += pow(nets[d].v(n_layers-1,0)-Train(10,0),2);
-				}
+	//	Define fully connected neural net : layer0 : 500 nodes / layer 1 : 250  nodes / layer 2 : 125 nodes / layer 3 : 1 node
 
-				if( min == -1 or min > err){
-//					cout << pow(N.v(4-1,0)-Train(10,0),2)<< endl;
-					min = err;
-					lmin = lin;
-				}
-				lin *= 10;
+	//	For layer 0 we will add 0s because we only have 486 assets.
+
+	vector<double(*)(const double &)> fs{ I, Lrelu, Lrelu,Lrelu };
+	vector<double(*)(const double &)> ds{ One, DLrelu, DLrelu, DLrelu};
+
+
+	Net N = Net(vector<int>{484, 242, 121, 1}, fs);
+	Net G = Net(vector<int>{484, 242, 121, 1}, ds); /* we do not care of the target we just want the structure*/
+	vec res_grad = vec(G.get_coeffs().n_rows, fill::zeros);
+
+	std::function<arma::mat(const arma::mat &)> g = [&Train, &N, &G, &res_grad, &end_train, &nassets](const arma::mat & v) {
+		N.get_coeffs() = v;
+		res_grad.fill(0.);
+		for (int d = 0; d != end_train + 1 - 10; ++d) {
+			//		For each available date, we calculate a gradient and then we average
+			N.n(0, 0) = 0;
+			for (int s = 1 /* do not use the current index price*/; s != nassets; ++s) {
+				N.n(0, s) = Train(d, s);
 			}
-			eth = lmin*eth;
-//			cout << lmin <<endl;
-
-			v = v-eth*g;
-
-
-			g = grad(v,gen);
-			if(i % 100000 == 0){
-			cout << norm(g) << '\n';
-			}
+			N.update();
+			//		cout <<N.v(3,0)<<endl;
+			grad(N, G, Train(d + 10, 0));
+			res_grad += G.get_coeffs();
 		}
-	}
-	cout << "reached max it" << norm(g) << '\n';
-	return v;
-}
-
-int main(){
-
-	nets.reserve(::end_train+1-10);
-	for(int d = 0 ; d != ::end_train+1-10 ; ++d){
-	//		For each available date, we calculate a gradient and then we average
-		nets.push_back(Net(vector<int>{487,250,125,1},fs));
-		nets[d].n(0,0) = 0;
-		for(int s = 1 /* do not use the current index price*/ ; s != nassets ; ++s){
-			nets[d].n(0,s) = Train(d,s);
-		}
-		nets[d].update();
-	}
-
-
-
-	std::function<arma::mat(const arma::mat &, mt19937 & )> g_st = [&](const arma::mat & v, mt19937 & g){
-	uniform_int_distribution<int> Dist(0,::end_train-10);
-//	const int batchSize{2};
-	res_grad.fill(0.);
-	int d{0};
-	for(int i = 0 ; i!= ::batchSize ; ++i){
-		d = Dist(g);
-//		cout << d<<endl;
-		::nets[d].get_coeffs() = v;
-		nets[d].update();
-		grad(nets[d],G,::Train(d+10,0));
-		res_grad += G.get_coeffs();
-	}
-//	cout << ::nets[0].v(4,0)<<endl;
-	return res_grad/(::batchSize);
+		//	cout << N.v(3,0)<<endl;
+		return res_grad / (end_train + 1 - 10);
 	};
-
-
-
-
-//	dataframe dv0(nets[0].get_coeffs().n_rows,1,"v5.csv",false);
-//	vec v0 = dv0.getData();
-	vec v0{nets[0].get_coeffs().n_rows,fill::randn};
-//	vec v0 = nets[0].get_coeffs();
-
+	dataframe dv0(N.get_coeffs().n_rows, 1, "v0.csv", false);
+	vec v0 = N.get_coeffs();
+	//	vec v0(N.get_coeffs().n_rows,fill::randn);
 
 	auto start = std::chrono::high_resolution_clock::now();
-	vec vinf = stochastic_descent(v0,g_st,0.00000000001,0.00000001);
+	vec vinf = grad_descent(v0, g, 0.00000000001, 0.01);
 	auto finish = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> elapsed = finish - start;
 
-	dataframe initVec{vinf};
-	initVec.write_csv("v6.csv");
+	dataframe initVec{ vinf };
+	initVec.write_csv("v1.csv");
 
-	cout << norm( v0 - vinf)/v0.n_rows <<endl;
-	cout << "Prediciton   Real Val"<<'\n';
-	double err = 0;
-	for(int d = 0 ; d != end_train+1-10 ; ++d){
-//		nets[d].get_coeffs() = v0;
-//		nets[d].update();
-		err += pow(nets[d].v(3,0) - Train(d+10,0),2);
-		cout << nets[d].v(3,0) << "           "<<Train(d+10,0)<<'\n';
+	cout << norm(v0 - vinf) / v0.n_rows << endl;
+
+	for (int d = 0; d != end_train + 1 - 10; ++d) {
+		//		For each available date, we calculate a gradient and then we average
+		N.n(0, 0) = 0;
+		for (int s = 1 /* do not use the current index price*/; s != nassets; ++s) {
+			N.n(0, s) = Train(d, s);
+		}
+		N.update();
+		cout << N.v(3, 0) << ' ' << Train(10 + d, 0) << ' ';
 	}
-	cout << err/( end_train+1-10);
-//	cout << "time" << elapsed.count();
-
-
+	cout << "time" << elapsed.count();
 	return 0;
 }
-
-
